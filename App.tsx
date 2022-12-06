82c0ddd17107b5d5560ba45f139994448946b032
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-12-05 21:38:18 -0300

    config: first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>POMODORO TIMER</Text>
+      <View style={styles.wrapper}>
+        <TextInput
+          style={styles.input}
+          placeholder='Insira sua tarefa:'
+          placeholderTextColor='#fff'
+        />
+        <Button color='#7fb16e' title='Iniciar' />
+      </View>
+      <View style={styles.timer}>
+        <Text style={styles.counter}>00</Text>
+        <Text style={styles.divider}>:</Text>
+        <Text style={styles.counter}>00</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +25,54 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    padding: 20,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1c1c1e'
   },
-});
+
+  wrapper: {
+    width: '100%',
+    marginBottom: 32,
+    padding: 16,
+    backgroundColor: '#6b7280',
+    borderRadius: 6
+  },
+
+  title: {
+    marginBottom: 16,
+    fontFamily: 'monospace',
+    fontSize: 35,
+    color: '#b1de44'
+  },
+
+  input: {
+    padding: 2,
+    marginBottom: 16,
+    borderBottomColor: '#b1de44',
+    borderBottomWidth: 1,
+    fontSize: 18
+  },
+
+  timer: {
+    padding: 24,
+    flexDirection: 'row',
+    alignItems: 'center',
+    fontSize: 42,
+  },
+
+  divider: {
+    marginLeft: 12,
+    marginRight: 12,
+    color: '#b1de44',
+    fontSize: 42,
+    fontWeight: 'bold'
+  },
+
+  counter: {
+    fontFamily: 'sans-serif',
+    fontSize: 64,
+    color:'#fff'
+  }
+})
